a1c24c66b006feacc5e2020423e7e799e307a1b2
Compmaster-tech <qualidadecompmaster03@gmail.com>
2025-04-14 16:12:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function darOrientacao() {
         resultado = 'cancelamento e ativação em 33 dias.';
     } else if (movimentacao.includes('reconsideração')) {
         resultado = 'Reconsideração de linha cancelada a mais de 33 dias remunera como uma nova alta no valor do plano reconsiderado, o mesmo que consta no carrinho. Porém reconsideração de linha suspensa não é remunerado.';
-    } else if (movimentacao.includes('para a direita')) {
-        resultado = 'Mova-se 1 passo para a direita.';
+    } else if (movimentacao.includes('alta') && movimentacao.includes('flexibilização')) {
+        resultado = 'Pode ser realizado uma nova alta em um movimento de flexibilização apenas se a linha que o cliente já possui na base siga com o mesmo plano ou sofra um downgrade.';
     } else {
         resultado = 'Desculpe, não entendi a movimentação. Tente descrever de outra forma.';
     }
